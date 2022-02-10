57cc820fedd03b690e05b0c05579ddbc31fdfd72
Stanislav Osichnuk <stanhjrpower@gmail.com>
2022-02-10 21:53:58 +0200

    add swagger_settings to django_settings

--- star_navi_test/settings.py
+++ star_navi_test/settings.py
@@ -174,4 +174,25 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
+
+SWAGGER_SETTINGS = {
+    'SHOW_REQUEST_HEADERS': True,
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    },
+    'USE_SESSION_AUTH': False,
+    'JSON_EDITOR': True,
+    'SUPPORTED_SUBMIT_METHODS': [
+        'get',
+        'post',
+        'put',
+        'delete',
+        'patch'
+    ],
+}
+
 APPEND_SLASH = False
